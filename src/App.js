37008c08ae43f68c5d7b1a49cf91528c530d3e89
Ryan Guide <ryan.guide@comcast.net>
2020-07-27 18:58:10 -0400

    Trying to raise the state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Box from "./lightbox";
 
 
 class App extends React.Component {
-  renderBox(i) {
-    return (
-      <Box 
-        value={i}
-        toggle={this.toggle}
-      />
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false
+    }
   }
   render() {
     return(
@@ -20,32 +18,100 @@ class App extends React.Component {
         <p>Click the boxes to turn off the lights. Whenever you click a box, that box, and the adjacent boxes will all change.</p>
         <div className="m">
         <div className="board">
-          {this.renderBox(0)}
-          {this.renderBox(1)}
-          {this.renderBox(2)}
-          {this.renderBox(3)}
-          <br></br>
-          {this.renderBox(4)}
-          {this.renderBox(5)}
-          {this.renderBox(6)}
-          {this.renderBox(7)}
-          <br></br>
-          {this.renderBox(8)}
-          {this.renderBox(9)}
-          {this.renderBox(10)}
-          {this.renderBox(11)}
-          <br></br>
-          {this.renderBox(12)}
-          {this.renderBox(13)}
-          {this.renderBox(14)}
-          {this.renderBox(15)}
-          <br></br>
+        <Box 
+        value={0}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={1}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={2}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={3}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <br />
+        <Box 
+        value={4}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={5}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={6}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={7}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <br />
+        <Box 
+        value={8}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={9}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={10}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={11}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <br />
+        <Box 
+        value={12}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={13}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value={14}
+        toggle={this.toggle}
+        on={this.state.on}
+        />
+        <Box 
+        value="15"
+        onClick={this.toggle}
+        on={this.state.on}
+        />
+        <br />
         </div>
         </div>
       </div>
     )
   }
-
+  toggle = () => {
+    this.setState(state => ({
+      [state.target.value]: !state.on
+    }))
+  }
 }
 
 export default App;
