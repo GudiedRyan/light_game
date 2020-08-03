1587d91ed3e97ae24e9a8c60760ed20c3cf36bd4
Ryan Guide <ryan.guide@comcast.net>
2020-08-03 17:23:21 -0400

    Updates

---
+++ precache-manifest.11199faa78748c6ed31db1c4fc3a8aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ac4ddacf22d6784d5b3f9c11a703bb",
+    "url": "/light_game/index.html"
+  },
+  {
+    "revision": "e361e70462bbb9fb34e0",
+    "url": "/light_game/static/css/main.5d0c7ce8.chunk.css"
+  },
+  {
+    "revision": "62e2a361079aaf77ff85",
+    "url": "/light_game/static/js/2.7d47ca6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/light_game/static/js/2.7d47ca6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e361e70462bbb9fb34e0",
+    "url": "/light_game/static/js/main.762b819b.chunk.js"
+  },
+  {
+    "revision": "8014edc6c06b2612d3be",
+    "url": "/light_game/static/js/runtime-main.64a12ac3.js"
+  }
+]);
